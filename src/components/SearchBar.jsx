b4279a92048fc4c231b90e7988b1b97231a0900d
LuisFalconS <125808185+Miguel7F@users.noreply.github.com>
2023-06-14 18:34:09 -0500

    Aplicando Wrap reverse

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,11 @@ import "./SearchBar.css"
 
 export default function SearchBar({ onSearch }) {
    const [id, setId] = useState("")
-   const randomNumber=Math.floor(Math.random()*825)+1
+
    function handleChange(event) {
       setId(event.target.value)
    }
+   const randomNumber=Math.floor(Math.random()*825)+1
 
    return (
       <div className="searchBar">
@@ -14,9 +15,6 @@ export default function SearchBar({ onSearch }) {
             <input type='search' onChange={handleChange} value={id} name="id" placeholder="Insert id..." />
             <button key="btSearch" onClick={() => { onSearch(id); setId("") }}>Agregar</button>
             <button key="btRandom" onClick={() => { onSearch(randomNumber)} }>Random</button>
-         
-
-
       </div>
    );
 }
